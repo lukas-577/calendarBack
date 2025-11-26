ad037e5adb7c4a6195ad17c4030565e06df3a5b9
lukas-577 <lukas_skyry_14@hotmail.cl>
2025-11-26 16:08:40 -0300

    ahora funciona prisma :D

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,14 @@ import { UserModule } from './user/user.module';
 import { FechasModule } from './fechas/fechas.module';
 import { LugaresModule } from './lugares/lugares.module';
 import { ReservaModule } from './reserva/reserva.module';
-import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,  // Hace que .env esté disponible en todo el proyecto
+    }),
     PrismaModule,
     UserModule,
     FechasModule,
